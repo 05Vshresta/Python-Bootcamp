ee04cb9057dfffafd6b95139f65dc4d069ef14e9
05Vshresta <vadlashresta@gmail.com>
2026-08-16 16:32:01 +0530

    Display books

--- Projects/library_management_system.py
+++ Projects/library_management_system.py
@@ -1,65 +1,88 @@
-#library_management_system
-
-
 import sqlite3
 
-#===========database connection========================
+# ============ DATABASE CONNECTION ========================
 conn = sqlite3.connect("library.db")
 cursor = conn.cursor()
+
 print("Database connected successfully")
 print("Welcome to the Library Management System")
 
-#============= Create the table=====================
+# ============ CREATE TABLE ===============================
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS books(
-book_id INTEGER PRIMARY KEY,
-title TEXT,
-author TEXT,
-category Text,
-quantity INTEGER,
-available INTEGER
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    category TEXT,
+    quantity INTEGER,
+    available INTEGER
 )
 """)
-print( " Books Table created successfully " )
-#================= ADD BOOK ==============================
+
+print("Books Table created successfully")
+
+
+# ============ ADD BOOK ====================================
 def add_book():
     title = input("Enter book title: ")
     author = input("Enter author name: ")
     category = input("Enter category: ")
-    quantity = input("Enter quantity:")
-    
+    quantity = int(input("Enter quantity: "))
 
     cursor.execute("""
     INSERT INTO books(
-    title,author,category,quantity,available)
-    VALUES(?, ?, ?, ?, ?)
-    """,(
-    title,
-    author,
-    category,
-    quantity,
-   
+        title, author, category, quantity, available
+    )
+    VALUES (?, ?, ?, ?, ?)
+    """, (
+        title,
+        author,
+        category,
+        quantity, # Total quantity of books
+        quantity  # Available quantity of books (initially same as total quantity)
     ))
+
     conn.commit()
 
     print("Book added successfully")
 
-#=====================================================
-    while True:
-        print("1. Add Book")
-        print("2. Exit")
+# ============ VIEW BOOKS ==================================
+def view_books():
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
 
-        choice = input("Enter your choice: ")
+    if len(books) == 0:
+        print("No Books Found")
+    else:
+        print("========== Available Books List ==============")
 
-        if choice == "1":
-            add_book()
+        for book in books:
+            print(f"""
+Book ID: {book[0]}
+Title: {book[1]}
+Author: {book[2]}
+Category: {book[3]}
+Quantity: {book[4]}
+Available: {book[5]}
+""")
 
-        elif choice == "2":
-            print("Thank you for visiting!")
-            conn.close()
-            break
+# ============ MAIN MENU ===================================
+while True:
+    print("\n1. Add Book")
+    print("2. View Books")
+    print("3. Exit")
 
-        else:
-            print("Invalid choice")
+    choice = input("Enter your choice: ")
 
+    if choice == "1":
+        add_book()
+    elif choice == "2":
+        view_books()
 
+    elif choice == "3":
+        print("Thank you for visiting!")
+        conn.close()
+        break
+
+    else:
+        print("Invalid choice")
