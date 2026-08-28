7507592e193c9d70e83d513d021345238d45b20c
05Vshresta <vadlashresta@gmail.com>
2026-08-28 22:32:13 +0530

    python basics

---
+++ Day-1/basics.py
@@ -0,0 +1,21 @@
+print("=" * 50)
+print("     Python learning Journey Day-1")
+print("=" * 50)
+
+name = input("👤 Enter your name: ")
+course = input("📚 Enter your course: ")
+goal = input(f"🎯 what is your {course} goal?  ")
+
+print("\n" + "=" * 50)
+print("          🚀 YOUR PYTHON PROFILE")
+print("=" * 50)
+
+print(f" Welcome, {name}!")
+print(f"📖 Course       : {course}")
+print(f"🎯 Goal         : {goal}")
+print("🔥 Status       : Day 1 — Journey Started!")
+print("💡 Mindset      : Learn → Practice → Build")
+
+print("=" * 50)
+print("🐍 Keep coding. Your journey starts here!")
+print("=" * 50)
