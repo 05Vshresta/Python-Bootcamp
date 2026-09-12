c3fdf67b719994dace43c00b38bb0f597d7a4fe7
05Vshresta <vadlashresta@gmail.com>
2026-09-12 15:47:14 +0530

    sum of first n Natural numbers

--- Day-9/sum_of_first_n_num.py
+++ Day-9/sum_of_first_n_num.py
@@ -1,6 +1,6 @@
-num = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 sum = 0
 
-for i in range(1,num+1):
+for i in range(1,number+1):
     sum += i
-print("Sum of first", num,"natural numbers is: ",sum)
+print("Sum of first", number,"Natural numbers is: ",sum)
