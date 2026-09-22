07067cbf5fbe4b40215201a2083d5ed32d0e82cc
05Vshresta <vadlashresta@gmail.com>
2026-09-22 19:32:47 +0530

    multiplication table

--- Day-19/mul_table.py
+++ Day-19/mul_table.py
@@ -1,6 +1,6 @@
 def multiplication_table(n, i=1):
     if i > 10:
-        return "Input should be less than or equal to 10"
+        return 
     print(f"{n} * {i} = {n*i}")
     multiplication_table(n, i+1)
 number = int(input("Enter a number: "))
